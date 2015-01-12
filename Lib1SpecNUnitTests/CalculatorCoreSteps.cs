1556118d646a704dc64661b17c213693e7ed5ca5
CityU Tomson Xu <tomsonxu@um.cityu.edu.hk>
2015-01-12 17:16:58 +0800

    add specflow test cases demo

---
+++ Lib1SpecNUnitTests/CalculatorCoreSteps.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using NUnit.Framework;
+
+namespace Lib1SpecNUnitTests
+{
+    [Binding]
+    public class CalculatorCoreSteps
+    {
+        [Given(@"I have entered (.*) into the calculator")]
+public void GivenIHaveEnteredIntoTheCalculator(int p0)
+{
+    Assert.AreEqual(1, 1);
+}
+
+        [When(@"I press add")]
+public void WhenIPressAdd()
+{
+    Assert.AreEqual(1, 1);
+}
+
+        [Then(@"the result should be (.*) on the screen")]
+public void ThenTheResultShouldBeOnTheScreen(int p0)
+{
+    Assert.AreEqual(1, 1);
+}
+    }
+}
